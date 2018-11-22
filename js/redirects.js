335807bdff7eaf49cb5cf0f7a5dcba0bb0c7877a
Sebastian <36368891+TheSebInside@users.noreply.github.com>
2018-11-22 23:15:09 +0100

    Create redirects.js

---
+++ js/redirects.js
@@ -0,0 +1,11 @@
+// Language: english
+function ClickEN()
+{
+  window.open("languages/english.html", "_self")
+}
+
+// Language: german
+function ClickDE()
+{
+  window.open("languages/german.html", "_self")
+}
